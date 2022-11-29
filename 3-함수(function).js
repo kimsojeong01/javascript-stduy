6837969271997035a81da3e320d6994e10961f9e
kimsojeong01 <ksj001207@naver.com>
2022-11-30 02:39:29 +0900

    학습

---
+++ 3-함수(function).js
@@ -0,0 +1,20 @@
+// 함수 선언
+function 함수이름() {
+  명령;
+  명령;
+}
+
+//함수선언은 그 함수가 사용될때 어떤 행동을 할지 정의만 된다.
+
+// 함수 선언
+function greetings() {
+  console.log('Hi');
+  console.log('안녕');
+  console.log('こんにちは');
+  console.log('你好');
+  console.log('Guten Tag');
+  console.log('Bonjour');
+  console.log('Buongiorno');
+}
+// 함수 호출
+greetings();
---
+++ 4-리턴(return).js
@@ -0,0 +1,10 @@
+// return 문
+// 다시 돌려준다 반환한다.
+
+// 예제 1번
+function getTwo() {
+  return 2;
+};
+
+console.log(getTwo()); // 2
+console.log(2); // 2
